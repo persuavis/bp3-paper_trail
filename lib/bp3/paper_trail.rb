51e1ec6688b4e66f20cf6903cb75e347c85da988
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 15:18:10 -0800

    Initial commit

---
+++ lib/bp3/paper_trail.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'paper_trail/version'
+
+module Bp3
+  module PaperTrail
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/bp3/paper_trail_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bp3::PaperTrail do
+  it 'has a version number' do
+    expect(Bp3::PaperTrail::VERSION).not_to be_nil
+  end
+
+  it 'does something useful' do
+    expect(false).to be(true)
+  end
+end
